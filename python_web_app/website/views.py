257169b45e722494cde346ea89ac02f7f8e8e126
Jan Hopkins <janhopkins1994@hotmail.com>
2022-03-17 09:34:18 +0000

    full project

---
+++ python_web_app/website/views.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from email import message
+from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponse
+from meetings.models import Meeting
+
+def welcome(request):
+    return render(request, "website/welcome.html", {"meetings": Meeting.objects.all()})
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("This is an about page")
